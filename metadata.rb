b95a68e388745ff9399f30f82fc021f36f64d326
Jeroen Jacobs <git@jeroenj.be>
2015-09-21 21:17:42 +0200

    Bumps version to v1.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Jeroen Jacobs'
 maintainer_email  'chef@jeroenj.be'
 license           'MIT'
 description       'Installs Plex Media Server.'
-version           '1.0.1'
+version           '1.0.2'
 
 depends           'apt'
 
